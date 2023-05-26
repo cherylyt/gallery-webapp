3a7e964e0face3f22c390b53e4acead355b5416b
Cheryl Chu <98683129+cherylyt@users.noreply.github.com>
2023-05-26 22:27:44 +0800

    changed icon and added linkedin

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Container, IconButton } from '@mui/material';
 import { createTheme } from '@mui/system';
 import { ThemeProvider } from '@mui/private-theming';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Tooltip from '@mui/material/Tooltip';
 
 const theme = createTheme({
   typography: {
@@ -25,9 +27,17 @@ function App() {
             <p >
               <span style={{color: '#D97D54'}}>Gallery </span>  <span style={{color: '#BDBDBD'}}>- digital and film pieces.</span>
               <span style={{float: 'right'}}>
-                <IconButton sx={{color: "#D97D54"}} onClick={() => window.open('https://github.com/cherylyt')}>
-                  <GitHubIcon />
-                </IconButton>
+                <Tooltip title = "LinkedIn">
+                  <IconButton sx={{color: "#D97D54"}} onClick={() => window.open('https://www.linkedin.com/in/yan-tung-cheryl-chu/')}>
+                    <LinkedInIcon />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title = "Github">
+                  <IconButton sx={{color: "#D97D54"}} onClick={() => window.open('https://github.com/cherylyt')}>
+                    <GitHubIcon />
+                  </IconButton>
+                </Tooltip>
+                <span style={{color: '#D97D54', fontSize: 15}}>Cheryl Chu</span>
               </span>
             </p>
           </Container>
